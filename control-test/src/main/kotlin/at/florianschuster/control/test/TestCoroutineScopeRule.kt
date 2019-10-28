ae3f5627daad936d04fdcba389b40397d0b4d6d7
Florian Schuster <mail@florianschuster.at>
2019-10-28 17:57:51 +0100

    Updated TestCoroutineScopeRule

--- control-test/src/main/kotlin/at/florianschuster/control/test/TestCoroutineScopeRule.kt
+++ control-test/src/main/kotlin/at/florianschuster/control/test/TestCoroutineScopeRule.kt
@@ -1,33 +1,38 @@
 package at.florianschuster.control.test
 
-import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestCoroutineDispatcher
 import kotlinx.coroutines.test.TestCoroutineScope
 import kotlinx.coroutines.test.resetMain
 import kotlinx.coroutines.test.setMain
 import org.junit.rules.TestRule
-import org.junit.rules.TestWatcher
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import kotlin.coroutines.ContinuationInterceptor
 
 /**
  * Rule that is a [TestCoroutineScope].
  * Coroutine's launched in [TestCoroutineScopeRule] are auto canceled after the test completes.
+ *
+ * @property overrideMainDispatcher Boolean if set to true, [Dispatchers.Main] will use
+ * [TestCoroutineDispatcher] of [TestCoroutineScope].
  */
 @ExperimentalCoroutinesApi
 class TestCoroutineScopeRule(
-    val mainDispatcher: CoroutineDispatcher? = null
+    val overrideMainDispatcher: Boolean = false
 ) : TestRule, TestCoroutineScope by TestCoroutineScope() {
+
+    val mainDispatcher = coroutineContext[ContinuationInterceptor] as TestCoroutineDispatcher
 
     override fun apply(base: Statement, description: Description): Statement =
         object : Statement() {
             @Throws(Throwable::class)
             override fun evaluate() {
-                if (mainDispatcher != null) Dispatchers.setMain(mainDispatcher)
+                if (overrideMainDispatcher) Dispatchers.setMain(mainDispatcher)
                 base.evaluate()
                 cleanupTestCoroutines()
-                if (mainDispatcher != null) Dispatchers.resetMain()
+                if (overrideMainDispatcher) Dispatchers.resetMain()
             }
         }
 }
