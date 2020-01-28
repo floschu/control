7d0c581f65fe6f407d47ec67b93c72e2a0aad351
Florian Schuster <mail@florianschuster.at>
2020-01-28 21:43:20 +0100

    Cleaned up gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,6 @@ buildscript {
     dependencies {
         classpath(Libs.kotlin_gradle_plugin)
         classpath(Libs.com_jfrog_bintray_gradle_plugin)
-
         classpath(Libs.com_android_tools_build_gradle)
         classpath(Libs.kotlin_serialization)
     }
@@ -16,8 +15,8 @@ buildscript {
 
 plugins {
     buildSrcVersions
+    jacoco
     id("org.jlleitschuh.gradle.ktlint").version(Versions.org_jlleitschuh_gradle_ktlint_gradle_plugin)
-    id("jacoco")
 }
 
 allprojects {
--- control-core/build.gradle.kts
+++ control-core/build.gradle.kts
@@ -1,35 +1,21 @@
-import org.jetbrains.kotlin.gradle.plugin.KotlinSourceSet
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
 plugins {
-    id("java-library")
     id("kotlin")
     id("jacoco")
-}
-
-val compileKotlin: KotlinCompile by tasks
-compileKotlin.kotlinOptions { jvmTarget = JavaVersion.VERSION_1_8.toString() }
-val compileTestKotlin: KotlinCompile by tasks
-compileTestKotlin.kotlinOptions { jvmTarget = JavaVersion.VERSION_1_8.toString() }
-
-sourceSets["main"].withConvention(KotlinSourceSet::class) { kotlin.srcDir("src/main/kotlin") }
-
-tasks {
-    getByName<JacocoReport>("jacocoTestReport") {
-        reports {
-            xml.isEnabled = true
-            html.isEnabled = false
-            csv.isEnabled = false
-        }
-    }
 }
 
 dependencies {
     api(Libs.kotlin_stdlib)
     api(Libs.kotlinx_coroutines_core)
-
     testImplementation(Libs.mockk)
     testImplementation(Libs.flow_extensions)
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.isEnabled = true
+        html.isEnabled = false
+        csv.isEnabled = false
+    }
 }
 
 apply(from = "$rootDir/gradle/deploy.gradle")
