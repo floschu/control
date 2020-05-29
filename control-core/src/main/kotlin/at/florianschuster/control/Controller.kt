87c09bf03c3db867dca33fecf575806b430017d1
Florian Schuster <mail@florianschuster.at>
2020-05-29 18:54:35 +0200

    Remove unused import

--- control-core/src/main/kotlin/at/florianschuster/control/Controller.kt
+++ control-core/src/main/kotlin/at/florianschuster/control/Controller.kt
@@ -7,7 +7,6 @@ import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.FlowPreview
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.flowOf
 import kotlin.coroutines.ContinuationInterceptor
 
