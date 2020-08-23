05357e078992206536d5a5a229bb7a5358793cac
Florian Schuster <mail@florianschuster.at>
2020-08-23 20:39:46 +0200

    Update Versions

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -12,20 +12,19 @@ import org.gradle.plugin.use.PluginDependencySpec
  * YOU are responsible for updating manually the dependency version.
  */
 object Versions {
-    const val org_jetbrains_kotlinx_kotlinx_serialization: String = "0.20.0"
-    // available: "0.20.0-1.4-M1-release-99"
+    const val org_jetbrains_kotlinx_kotlinx_serialization: String = "1.0.0-RC"
 
-    const val org_jetbrains_kotlinx_kotlinx_coroutines: String = "1.3.8"
+    const val org_jetbrains_kotlinx_kotlinx_coroutines: String = "1.3.9"
 
     const val io_github_reactivecircus_flowbinding: String = "0.12.0"
 
-    const val org_jetbrains_kotlin: String = "1.3.72"
+    const val org_jetbrains_kotlin: String = "1.4.0"
 
     const val androidx_fragment: String = "1.2.5"
 
     const val androidx_test: String = "1.2.0"
 
-    const val io_ktor: String = "1.3.2" // available: "1.3.2-1.4-M1-release-99"
+    const val io_ktor: String = "1.4.0"
 
     const val com_android_tools_build_gradle: String = "4.0.1"
 
@@ -39,40 +38,40 @@ object Versions {
 
     const val coroutines_test_extensions: String = "0.1.2"
 
-    const val atomicfu_gradle_plugin: String = "0.14.3"
+    const val atomicfu_gradle_plugin: String = "0.14.4"
 
     const val lifecycle_runtime_ktx: String = "2.2.0"
 
-    const val gradle_pitest_plugin: String = "1.5.1"
+    const val gradle_pitest_plugin: String = "1.5.2"
 
-    const val constraintlayout: String = "2.0.0-beta4"
+    const val constraintlayout: String = "2.0.0"
 
     const val espresso_core: String = "3.2.0"
+
+    const val atomicfu_jvm: String = "0.14.4"
 
     const val lint_gradle: String = "27.0.1"
 
     const val viewbinding: String = "4.0.1"
 
-    const val appcompat: String = "1.1.0"
+    const val appcompat: String = "1.2.0"
 
     const val junit_ktx: String = "1.1.1"
 
-    const val atomicfu: String = "0.14.3"
-
-    const val material: String = "1.1.0"
+    const val material: String = "1.2.0"
 
     const val ktlint: String = "0.37.2"
 
-    const val aapt2: String = "4.0.0-6051327" // available: "4.0.1-6197926"
+    const val aapt2: String = "4.0.1-6197926"
 
     const val mockk: String = "1.10.0"
 
     /**
-     * Current version: "6.5.1"
+     * Current version: "6.6"
      * See issue 19: How to update Gradle itself?
      * https://github.com/jmfayard/buildSrcVersions/issues/19
      */
-    const val gradleLatestVersion: String = "6.5.1"
+    const val gradleLatestVersion: String = "6.6"
 }
 
 /**
@@ -81,4 +80,4 @@ object Versions {
  */
 val PluginDependenciesSpec.buildSrcVersions: PluginDependencySpec
     inline get() =
-        id("de.fayard.buildSrcVersions").version(Versions.de_fayard_buildsrcversions_gradle_plugin)
+            id("de.fayard.buildSrcVersions").version(Versions.de_fayard_buildsrcversions_gradle_plugin)
