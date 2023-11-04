c094ead398462883baeac4d26c3c2fa4e3b3def8
Florian Schuster <mail@florianschuster.at>
2023-11-04 16:08:29 +0100

    Update versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,15 +6,15 @@ buildscript {
     }
 
     dependencies {
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
 
-        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.7.4")
+        classpath("info.solidsoft.gradle.pitest:gradle-pitest-plugin:1.15.0")
         classpath("org.jetbrains.kotlinx:binary-compatibility-validator:0.13.2")
-        classpath("com.vanniktech:gradle-maven-publish-plugin:0.19.0")
-        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.0")
+        classpath("com.vanniktech:gradle-maven-publish-plugin:0.25.3")
+        classpath("org.jetbrains.dokka:dokka-gradle-plugin:1.9.10")
 
-        classpath("com.android.tools.build:gradle:8.0.2")
-        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.0")
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("org.jetbrains.kotlin:kotlin-serialization:1.9.20")
     }
 }
 
